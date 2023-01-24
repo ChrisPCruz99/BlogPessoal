219a19ee8e346adc539807832826af63c91ad25e
unknown <christian.pcruz99@gmail.com>
2023-01-24 18:48:11 -0300

    Criação do CRUD Blog Pessoal

---
+++ blogpessoal/src/main/java/org/blogpessoal/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,25 @@
+package org.blogpessoal.generation.blogpessoal.controller;
+
+import java.util.List; 
+import org.blogpessoal.generation.blogpessoal.model.Postagem;
+import org.blogpessoal.generation.blogpessoal.repository.PostagemRepository; 
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin; 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController; 
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+}
---
+++ blogpessoal/src/main/java/org/blogpessoal/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,55 @@
+package org.blogpessoal.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+// para fazer virar uma tabela
+
+@Entity
+@Table(name="tb_postagens")
+public class Postagem {
+	
+	//primary key
+	@Id
+	//auto_increment e decisão de como sera incrementado
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	@Size(min = 5, max = 100)
+	public String titulo;
+	@NotNull
+	@Size(min = 10, max = 500)
+	public String texto;
+	public LocalDateTime date;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDateTime getDate() {
+		return date;
+	}
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+}
